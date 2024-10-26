d9feae2b4784a542c6fa92041ee7510197a39e29
Edugonzalez <eduu.gonzalez@gmail.com>
2024-10-26 13:12:21 +0200

    Ejercicio 4 Completado

--- src/main/java/com/campusdual/classroom/Exercise04.java
+++ src/main/java/com/campusdual/classroom/Exercise04.java
@@ -9,22 +9,54 @@ public class Exercise04 {
         System.out.println("Obtén el factorial del número 7 → (7!): " + (7 * 6 * 5 * 4 * 3 * 2 * 1));
         System.out.println("######################################################################");
         System.out.println("Ejemplos de cálculos CON estructuras de control:");
+        System.out.println("Suma de los primeros 5 números: " + sumNum(5));
+        //Exercise04.sumNum(5);
+        System.out.println("Suma de los primeros 20 números pares: " + sumEvenNum(20));
+        //Exercise04.sumEvenNum(40);
+        System.out.println("Obtén el factorial del número 7 → (7!): " + factorial(7));
+        //System.out.println(recursiveFactorial(7));
+        System.out.println(recursiveFactorial(7));
 
     }
 
     public static int sumNum(int num) {
-        return 0;
+        int total = 0;
+        for (int i = 1; i <= num ; i++) {
+          total = total + i;
+            //System.out.println(x);
+        }
+    return total;
     }
 
+
     public static int sumEvenNum(int num) {
-        return 0;
+        int x = 0;
+        for (int i = 2; i <= (num * 2) ; i+=2) {
+            x = x + i;
+            //System.out.println(x);
+
+        }
+        return x;
     }
 
     public static int factorial(int num) {
-        return 0;
+        int x= 1;
+        for (int i = 1; i <= num ; i++) {
+            x *=i;
+        }
+
+        return x;
     }
 
-    public static int recursiveFactorial(int num) {
-        return 0;
+   public static int recursiveFactorial(int num) {
+       if(num == 0){
+           return 1;
+       } else {
+           return num * recursiveFactorial(num - 1);
+       }
     }
+
 }
+
+
+
